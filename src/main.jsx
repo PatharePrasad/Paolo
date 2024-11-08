e12a1be91db00683280a1628239b6c11a4ecf9e9
Prasad Pathare <prasadsachinist10@gmail.com>
2024-11-08 09:46:29 +0000

    Accordion Implementation

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import App from "./App.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
